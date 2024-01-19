badc7471528f4033028e17bf42a2f7dc91188914
Chekhanadski Andrei <chekhanadski@gmail.com>
2024-01-19 17:15:35 +0100

    feat: update path for file text.txt

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-fs.readFile('./text.txt', 'utf8', (err, data) => {
+fs.readFile('01-read-file/text.txt', 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading the file:', err);
     return;
   }
-  console.log('File content:', data);
+  console.log(data);
 });
