9ac7c829af927175aeca2391a94a7f074985b9f8
Chekhanadski Andrei <chekhanadski@gmail.com>
2024-01-24 08:13:12 +0100

    feat: change "01-read-file" task, solve use ReadStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-fs.readFile('01-read-file/text.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading the file:', err);
-    return;
-  }
-  console.log(data);
-});
+const readStream = fs.createReadStream('01-read-file/text.txt', 'utf-8');
+
+readStream.on('data', function(chunk) {
+  console.log(chunk);
+}).on('error', function(err) {
+  console.error('Error reading the file:', err);
+})
